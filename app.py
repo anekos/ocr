f5774c5ab1ad0d72063172fd7c8189cde46b19bd
anekos <anekos@snca.net>
2022-05-09 22:51:19 +0900

    Remove blank lines

--- app.py
+++ app.py
@@ -27,16 +27,25 @@ def remove_mysterious_spaces(s: str) -> str:
     return ''.join(result)
 
 
+def remove_blank_lines(s: str) -> str:
+    return '\n'.join(filter(lambda it: not len(it) == 0, regex.split('\n', s)))
+
+
 # tesseract_layout - https://valmore.work/how-to-use-tesseract4-with-python/
 
 @click.command()
 @click.argument('image_path', type=str, required=True)
 @click.argument('layout', type=int, default=12)
 @click.argument('lang', type=str, default='jpn')
-def main(image_path: str, layout: int, lang: str) -> None:
+@click.option('--cleanup', type=bool, default=True)
+def main(image_path: str, layout: int, lang: str, cleanup: bool) -> None:
     image = Image.open(image_path)
     builder = pyocr.builders.TextBuilder(tesseract_layout=layout)
     text = ocr.image_to_string(image, lang='jpn', builder=builder)
+    if cleanup:
+        text = remove_mysterious_spaces(text)
+        text = remove_blank_lines(text)
+    print(text)
 
 
 if __name__ == "__main__":
